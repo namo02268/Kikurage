f1030081dc9622a1205f7445070fd14e18acf1da
namo02268 <namo02268@gmail.com>
2022-04-28 14:10:39 +0900

    [Update] Optimize getComponent

--- Kikurage/src/Kikurage/ECS/EntityMap.h
+++ Kikurage/src/Kikurage/ECS/EntityMap.h
@@ -7,15 +7,17 @@
 
 class EntityMap {
 private:
-	std::unordered_map<EntityID, ComponentInstance> m_entityToInstance;
+	std::array<ComponentInstance, MAX_ENTITIES> m_entityToInstance;
 	std::array<EntityID, MAX_ENTITIES> m_instanceToEntity;
 
 public:
+	~EntityMap() = default;
+
 	Entity getEntity(ComponentInstance i) { return m_instanceToEntity[i]; }
 	ComponentInstance getInstance(Entity e) { return m_entityToInstance[e.GetID()]; }
 
 	void add(Entity& e, ComponentInstance i) {
-		m_entityToInstance.insert({ e.GetID(), i });
+		m_entityToInstance[e.GetID()] = i;
 		m_instanceToEntity[i] = e.GetID();
 	}
 
@@ -23,5 +25,4 @@ public:
 		m_entityToInstance[e.GetID()] = i;
 		m_instanceToEntity[i] = e.GetID();
 	}
-	void remove(Entity& e) { m_entityToInstance.erase(e.GetID()); }
 };
